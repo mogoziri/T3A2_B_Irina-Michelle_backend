6b33bc70c42f2ad699e9d696218299e3dd6381d4
Irina Mogozova <12902@coderacademy.edu.au>
2023-02-22 19:56:39 +1100

    Add reservations model, seed.js

--- src/seed.js
+++ src/seed.js
@@ -1,15 +1,45 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
+require("dotenv").config()
 
 const Admin = require("./models/admin")
+const User = require("./models/user")
+const Owner = require("./models/owner")
+const Vehicle = require("./models/vehicle")
 
-mongoose.connect("mongodb://127.0.0.1:27017/carental", async () => {
+mongoose.connect(process.env.MONGO_URI, async () => {
   await Admin.deleteMany({})
-  const hashedPassword = await bcrypt.hash("password", 10)
+  const adminHashedPassword = await bcrypt.hash("password", 10)
   const admin = await Admin.create({
     username: "admin",
-    password: hashedPassword,
+    password: adminHashedPassword,
   })
   console.log(admin)
+  await User.deleteMany({})
+  const userHashedPassword = await bcrypt.hash("password", 10)
+  const user = await User.create({
+    username: "user",
+    password: userHashedPassword,
+  })
+  console.log(user)
+  await Owner.deleteMany({})
+  const ownerHashedPassword = await bcrypt.hash("password", 10)
+  const owner = await Owner.create({
+    username: "owner",
+    password: ownerHashedPassword,
+  })
+  console.log(owner)
+  await Vehicle.deleteMany({})
+  const vehicle = await Vehicle.create({
+    type: "SUV",
+    owner_id: owner._id,
+    price_per_day: 90,
+    location: "Summer Hill, NSW",
+    availability: true,
+    description: "Volkswagen Tiguan 2018",
+  })
+  console.log(vehicle)
+
+  // optional: add user rating, vehicle rating and reservation
   mongoose.connection.close()
 })
